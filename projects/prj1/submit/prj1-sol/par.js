df2985a06e3a19f4d21faa9d70b7d050dca37f7c
nikhil-raavi <sravi3@binghamton.edu>
2023-02-13 22:09:46 -0500

    final commit

--- projects/prj1/submit/prj1-sol/par.js
+++ projects/prj1/submit/prj1-sol/par.js
@@ -1,116 +1,109 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function parse(inp) {
-  let tokens = inp.match(/\b\w+\b|[{};:^#]/g);
-  let pos = 0;
-  let result = [];
+function main(inp) {
+  let d = 0;
+  let final = [];
+  let token_stream = inp.match(/\b\w+\b|[{};:^#]/g);
 
-  function parseDeclaration() {
-    if (tokens[pos] === 'var') {
-      pos += 1;
-      let varName = tokens[pos];
-      if (varName === 'number' || varName === 'string' || varName === 'record') {
-        throw new Error(`Error: expecting 'id' but got '${varName}'`);
-      } 
-      pos += 1;
-      if (tokens[pos] === ':') {
-        pos += 1;
-        varType = parse_type();
-        result.push([varName, varType]);
+  function declare() {
+    if (token_stream[d] === "var") {
+      d += 1;
+      let varName = token_stream[d];
+      if (
+        varName === "number" ||
+        varName === "string" ||
+        varName === "record"
+      ) {
+        throw new Error(`Error! No 'id' is read`);
+      }
+      d += 1;
+      if (token_stream[d] === ":") {
+        d += 1;
+        varType = check_types();
+        final.push([varName, varType]);
       } else {
-        throw new Error("Error: Either Missing colon ':' or TypeId in the input");
+        throw new Error("Error! ':' or 'Typeid' is missing");
       }
-    } else if (tokens[pos] === ';') {
-      pos += 1;
-      if (pos < tokens.length - 1) {
-        parseDeclaration();
+    } else if (token_stream[d] === ";") {
+      d += 1;
+      if (d < token_stream.length - 1) {
+        declare();
       }
     } else {
-      throw new Error("Error: missing 'var");
+      throw new Error("Error!");
     }
   }
 
-  function parse_type() {
-    if (tokens[pos] === 'number') {
-        pos++;
-        if (pos >= tokens.length || tokens[pos] !== ';') {
-            throw new Error("Error: field is missing semi-colon ';'");
+  function check_types() {
+    if (token_stream[d] === "number") {
+      d++;
+      if (d >= token_stream.length || token_stream[d] !== ";") {
+        throw new Error("Error! ';' is missing");
+      }
+      return "number";
+    } else if (token_stream[d] === "string") {
+      d++;
+      if (d >= token_stream.length || token_stream[d] !== ";") {
+        throw new Error("Error! ';' is missing");
+      }
+      return "string";
+    } else if (token_stream[d] === "record") {
+      d++;
+      let record = [];
+      while (token_stream[d] !== "end") {
+        if (token_stream[d] === ";") {
+          d++;
         }
-        return 'number';
-    } else if (tokens[pos] === 'string') {
-        pos++;
-        if (pos >= tokens.length || tokens[pos] !== ';') {
-            throw new Error("Error: field is missing semi-colon ';'");
+        if (token_stream[d] === "end") {
+          break;
         }
-        return 'string';
-    } else if (tokens[pos] === 'record') {
-        pos++;
-        let record = [];
-        while (tokens[pos] !== 'end') {
-            if (tokens[pos] === ';') {
-                pos++;
-            }
-            if (tokens[pos] === 'end') {
-                break;
-            }
-            if (pos < tokens.length - 1) {
-                let var_name = tokens[pos];
-                if (['number', 'string', 'record'].includes(var_name)) {
-                    throw new Error(`Error: expecting 'id' but got '${var_name}'`);
-                }
-                pos++;
-                if (tokens[pos] === ':') {
-                    pos++;
-                    let var_type = parse_type();
-                    record.push([var_name, var_type]);
-                } else {
-                    throw new Error("Error: field Missing colon ':'");
-                }
-            }
+        if (d < token_stream.length - 1) {
+          let var_name = token_stream[d];
+          if (["number", "string", "record"].includes(var_name)) {
+            throw new Error(`Error! No 'id' is read`);
+          }
+          d++;
+          if (token_stream[d] === ":") {
+            d++;
+            let var_type = check_types();
+            record.push([var_name, var_type]);
+          } else {
+            throw new Error("Error! ':' is missing");
+          }
         }
-        pos++;
-        if (record.length === 0) {
-            throw new Error("Error: Empty record in the Input");
-        } else {
-            return record;
-        }
+      }
+      d++;
+      if (record.length === 0) {
+        throw new Error("Error! No Input");
+      } else {
+        return record;
+      }
     } else {
-        throw new Error('Error: Invalid type, Input has no type');
-        }
+      throw new Error("Error !Input has no type");
     }
-  while (pos < tokens.length - 1) {
-    parseDeclaration();
   }
-    
-    return result;
+  while (d < token_stream.length - 1) {
+    declare();
+  }
+  return final;
 }
-var lines = [];
-var d = [];
 
-fs.readFile('input.txt', 'utf-8', (err, data) => {
+fs.readFile("input.txt", "utf-8", (err, data) => {
   if (err) throw err;
-  console.log(data,)
-  const b = data.split('\n');
-  console.log(b)
-  for (var i = 0; i < b.length; i++) {
-  if (b[i].includes("\n")) {
-  b[i] = b[i].split("\n")[0];
+  const inp_tx = data.split("\n");
+  for (var i = 0; i < inp_tx.length; i++) {
+    if (inp_tx[i].includes("\n")) {
+      inp_tx[i] = inp_tx[i].split("\n")[0];
+    }
+    if (inp_tx[i].includes("\t")) {
+      inp_tx[i] = inp_tx[i].split("\t")[1];
+    }
+    var inp1 = inp_tx[i].split("#");
+    inp_tx[i] = inp1[0];
+    if (inp_tx[i].includes(".")) {
+      throw new Error("Error! Invalid stream");
+    }
   }
-  if (b[i].includes("\t")) {
-  b[i] = b[i].split("\t")[1];
-  }
-  var f = b[i].split("#");
-  b[i] = f[0];
-  if (b[i].includes(".")) {
-  throw new Error("Error: Bad character");
-  }
-  }
-  var res1 = parse(b.toString());
-  console.log(JSON.stringify(res1, null, 4));  // Do your processing here
+  var conv_res = main(inp_tx.toString());
+  console.log(JSON.stringify(conv_res, null, 4)); // Do your processing here
 });
-
-
-
-
-
-
